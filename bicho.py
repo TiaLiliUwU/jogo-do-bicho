0eeb4f1552c2cdbf6ec56f67bb6799d813acd8ba
TiaLiliUwU <henderson12souza@gmail.com>
2023-06-09 23:16:14 -0300

    desenvolvendo as apostas

--- bicho.py
+++ bicho.py
@@ -49,26 +49,43 @@ animais = {
     "Vaca": range(97, 100),
 }
 
+# função duro ou cercado
+def duro_cerc():
+    clear()
+    print("Que tipo de aposta deseja fazer? \n\n1 - Duro\n2 - Cercado")
+    duro_cercado = int(input("\nSelecione a alternativa: "))
+    tipo = True
+    if duro_cercado == 1:
+        tipo = True
+    elif duro_cercado == 2:
+        tipo = False
+    else:
+        erro()
+        enter()
+        return duro_cerc()
+    return tipo
+
 # Função para realizar o sorteio
 def sortear():
-    animal = random.choice(list(animais.keys()))  # Escolhe um animal aleatoriamente
-    numero = random.choice(animais[animal])  # Escolhe um número aleatoriamente para o animal escolhido
-    return animal, numero
-
-#Função para verificar se o animal está dentro da tabela
-#def verific_animal():
-    while True:
-        if verific_ani in range(len(animais)):
-            break
-        else:
-            erro()
-            enter()
-            clear()
-            print("\nOpção inválida. Tente novamente.")
+    sorteios_ani = []
+    while len(sorteios_ani) < 5:
+        animal = random.choice(list(animais.keys()))  # Escolhe um animal aleatoriamente
+        if animal not in sorteios_ani:
+            sorteios_ani.append(animal)
+    sorteios_num = []
+    while len(sorteios_num) < 5:
+        numero = random.choice(range(0, 9999))  # Escolhe um número aleatoriamente para o animal escolhido
+        numero_formatado = str(numero).zfill(4)
+        if numero_formatado not in sorteios_num:
+            sorteios_num.append(numero_formatado)
+    
+    return sorteios_ani, sorteios_num
 
 # Função "Aposta no grupo"
 def grupo():
-    print("\nEm qual grupo deseja apostar? \n")
+    tipo_aposta = duro_cerc()
+    clear()
+    print("Em qual grupo deseja apostar? \n")
     for indice, animal in enumerate(animais, start=1):
         print(f"{indice}. {animal}")
     animal_escolhido = int(input("\nDigite o número correspondente ao animal desejado: ")) - 1
@@ -79,10 +96,10 @@ def grupo():
         enter()
         clear()
         return grupo()
-    return animal_escolhido
+    return animal_escolhido, tipo_aposta
 
-# Função "Aposta no duque grupo"
-def grupo():
+# Função "Aposta no duque do grupo"
+def duque_grupo():
     print("\nEm quais grupos deseja apostar? \n")
     for indice, animal in enumerate(animais, start=1):
         print(f"{indice}. {animal}")
@@ -95,54 +112,146 @@ def grupo():
         clear()
         return grupo()
     animal_escolhido_2 = int(input("\nDigite o número correspondente ao animal desejado: ")) - 1
-    return animal_escolhido_1, animal_escolhido_2
-
-
-# Função para solicitar a aposta do jogador
-def fazer_aposta():
-    print("Animais disponíveis: \n")
-    for indice, animal in enumerate(animais, start=1):
-        print(f"{indice}. {animal}")
-    animal_escolhido = int(input("\nDigite o número correspondente ao animal desejado: ")) - 1
-    if animal_escolhido in range(len(animais)):
-        range_animal = list(animais.values())[animal_escolhido]
-    else:
-        erro()
-        enter()
-        clear()
-        return fazer_aposta()
-    valor_aposta = float(input("\nDigite o valor da aposta: "))
-    if valor_aposta in range_animal:
+    if animal_escolhido_2 in range(len(animais)):
         pass
     else:
         erro()
         enter()
         clear()
-        return fazer_aposta()
-    return animal_escolhido, valor_aposta
+        return grupo()
+    return animal_escolhido_1, animal_escolhido_2
 
-# Função para verificar se o jogador ganhou ou perdeu
-def verificar_resultado(aposta, resultado):
-    animal_apostado_num, _ = aposta # Isso tá errado de acordo com a lógica do jogo, tem que refazer.
-    animal_apostado = list(animais.keys())[animal_apostado_num]
-    animal_sorteado, numero_sorteado = resultado
-    if animal_apostado.lower() == animal_sorteado.lower():
+# Função "Aposta no terno do grupo"
+def terno_grupo():
+    print("\nEm quais grupos deseja apostar? \n")
+    for indice, animal in enumerate(animais, start=1):
+        print(f"{indice}. {animal}")
+    animal_escolhido_1 = int(input("\nDigite o número correspondente ao animal desejado: ")) - 1
+    if animal_escolhido_1 in range(len(animais)):
+        pass
+    else:
+        erro()
+        enter()
+        clear()
+        return grupo()
+    animal_escolhido_2 = int(input("\nDigite o número correspondente ao animal desejado: ")) - 1
+    if animal_escolhido_2 in range(len(animais)):
+        pass
+    else:
+        erro()
+        enter()
+        clear()
+        return grupo()
+    animal_escolhido_3 = int(input("\nDigite o número correspondente ao animal desejado: ")) - 1
+    if animal_escolhido_3 in range(len(animais)):
+        pass
+    else:
+        erro()
+        enter()
+        clear()
+        return grupo()
+    return animal_escolhido_1, animal_escolhido_2, animal_escolhido_3
+
+
+# Função para solicitar a aposta do jogador
+def fazer_aposta():
+    print("Qual aposta você quer realizar? \n\n1 - Grupo \n2 - Duque de grupo \n3 - Terno de grupo\n")
+    tipo_aposta = int(input("Selecione a opção desejada: "))
+    if tipo_aposta == 1:
+        fezes = grupo()
+        verific_tipo_aposta = 0
+    elif tipo_aposta == 2:
+        fezes = duque_grupo()
+        verific_tipo_aposta = 1
+    elif tipo_aposta == 3:
+        verific_tipo_aposta = 2
+        fezes = terno_grupo()
+    else:
+        erro()
+    return fezes, verific_tipo_aposta
+    #for indice, animal in enumerate(animais, start=1):
+    #    print(f"{indice}. {animal}")
+    #animal_escolhido = int(input("\nDigite o número correspondente ao animal desejado: ")) - 1
+    #if animal_escolhido in range(len(animais)):
+    #    range_animal = list(animais.values())[animal_escolhido]
+    #else:
+    #    erro()
+    #    enter()
+    #    clear()
+    #    return fazer_aposta()
+    #valor_aposta = float(input("\nDigite o valor da aposta: "))
+    #if valor_aposta in range_animal:
+    #    pass
+    #else:
+    #    erro()
+    #    enter()
+    #    clear()
+    #    return fazer_aposta()
+    #return animal_escolhido, valor_aposta
+
+# Função para verificar aposta no duro/grupo
+def duro_grupo(animal_apostado, animal_sorteado, numero_sorteado):
+    if animal_sorteado[0] == animal_apostado:
         print("Parabéns! Você ganhou!")
         print(f"O animal sorteado foi {animal_sorteado} e o número sorteado foi {numero_sorteado}.")
     else:
         print("Que pena! Você perdeu.")
         print(f"O animal sorteado foi {animal_sorteado} e o número sorteado foi {numero_sorteado}.")
 
+# Função para verificar aposta cercado/grupo
+def cerc_grupo(animal_apostado, animal_sorteado, numero_sorteado):
+    if any(item in animal_apostado for item in animal_sorteado):
+        print("Parabéns! Você ganhou!")
+        print(f"O animal sorteado foi {animal_sorteado} e o número sorteado foi {numero_sorteado}.")
+    else:
+        print("Que pena! Você perdeu.")
+        print(f"O animal sorteado foi {animal_sorteado} e o número sorteado foi {numero_sorteado}.")
+
+# Função para verificar aposta no duque do grupo
+def duque_gp():
+    pass
+
+# Função para verificar se o jogador ganhou ou perdeu
+def verificar_resultado(aposta, resultado):
+    dds, verific_tipo_aposta = aposta
+    if dds[1] == True:
+        a1, a2 = dds 
+    elif dds[1] == False:
+        a1, a2 = dds 
+    elif len(dds) == 3:
+        a1, a2, a3 = dds 
+    animal_sorteado, numero_sorteado = resultado
+    if verific_tipo_aposta == 0: # No grupo
+        animal_apostado = list(animais.keys())[a1]
+        if a2 == True: # aposta no duro
+            duro_grupo(animal_apostado, animal_sorteado,numero_sorteado)
+        elif a2 == False: # aposta no cercado
+            cerc_grupo(animal_apostado, animal_sorteado, numero_sorteado)
+    elif verific_tipo_aposta == 1: # No duque de grupo
+        animal_apostado = []
+        animal_apostado_temp = list(animais.keys())[a1]
+        animal_apostado.append(animal_apostado_temp)
+        animal_apostado_temp = list(animais.keys())[a2]
+        animal_apostado.append(animal_apostado_temp)
+        print(animal_apostado) #debug
+        enter() #debug
+        duque_gp()
+
 # Função principal do jogo
 def jogar_jogo_do_bicho():
     print("Bem-vindo ao Jogo do Bicho! \n")
     aposta = fazer_aposta()
+    print(aposta) #debug
+    enter() #debug
     resultado = sortear()
+    print(resultado) #debug
+    enter() #debug
     verificar_resultado(aposta, resultado)
 
 # ______Execução do jogo______
 
 clear()
+print('Estamos em teste alpha, nem sabemos oq vai funcionar ou não. Paciência')
 jogar_jogo_do_bicho()
 enter()
 clear()
